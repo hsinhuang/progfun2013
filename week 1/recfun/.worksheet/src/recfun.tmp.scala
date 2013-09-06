c7908dd9723ae19f8e438901870049ff3fe27760
leon-huang <leon@njuopen.com>
2013-09-06 12:57:27 +0800

    all the solutions

---
+++ week 1/recfun/.worksheet/src/recfun.tmp.scala
@@ -0,0 +1,6 @@
+package recfun
+
+object tmp {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(72); 
+  println("Welcome to the Scala worksheet");$skip(12); 
+  var a = 0;System.out.println("""a  : Int = """ + $show(a ))}
+}
